bec2256f88dd224b6f126547173587248a147bd5
MatijaKukec <matija.kuk5@gmail.com>
2021-06-07 10:17:03 +0200

    Uređivanje1

--- Python_possible_passwords_generator.py
+++ Python_possible_passwords_generator.py
@@ -43,7 +43,7 @@ def crack1(slova, len):
   
     # call for all required lengths 
     for i in range(1 , len + 1):  
-        generate(slova, i, "", len) 
+        generate(slova, i, "", len)
 
 def crack2(slova, len): 
 
@@ -58,17 +58,24 @@ s = []
 pref=0
 xlen=0
 
-prefTrue = str(input("Želite li unijeti prefiks? (da/ne, yes/no) : "))
-if prefTrue[:1]=="y" or prefTrue[:1]=="d" or prefTrue[:1]=="D" or prefTrue[:1]=="Y":
-    pref = str(input("Unesite prefiks koji će se spajati: "))
 
-# number of elemetns as input 
-brSlov = int(input("Koliko elemenata želite unijeti? : "))
+inn=True
+while(inn):
+    prefTrue = str(input("Želite li unijeti prefiks? (da/ne, yes/no) : "))
+    if prefTrue[:1]=="y" or prefTrue[:1]=="d" or prefTrue[:1]=="D" or prefTrue[:1]=="Y":
+        pref = str(input("Unesite prefiks koji će se spajati: "))
+        inn=False
+    elif prefTrue[:1]=="n" or prefTrue[:1]=="N" : inn=False
+
+
+
+# number of elements as input 
+brSlov = int(input("Koliko elemenata želite unijeti?: "))
 # mesta = int(input("Koliko mjesta za moguću kombinaciju: "))
   
 # iterating till the range 
 for i in range(0, brSlov): 
-    ele = str(input())
+    ele = str(input("Upišite " +str(i+1)+ ". element: "))
     f.write(ele)
     slova.append(ele) # dodavanje elementa
     
@@ -77,14 +84,18 @@ q=x^brSlov
 
 print (str(q) + " mogućih kombinacija.\n")
 
-fixTrue = str(input("Želite li da se ispisuju lozinke od apsolutno svih kombinacija ili samo fiksne duljine, Fixsno/Ne?"))
-if fixTrue[:1]=="F" or fixTrue[:1]=="f" : 
-    lenTrue = str(input("Želite li da se ispisuju lozinke određene duljine? (da/ne, yes/no) :"))
-    if lenTrue[:1]=="y" or lenTrue[:1]=="d" or lenTrue[:1]=="D" or lenTrue[:1]=="Y":
-        crack2(slova, x)
-        xlen = int(input("Upišite željenu duljinu?"))
-    else: crack0(slova, x)
-else: crack1 (slova, x)
+
+inn=True
+while(inn):
+    fixTrue = str(input("Želite li da se ispisuju lozinke od apsolutno svih kombinacija ili samo fiksne duljine, Fixsno/Ne?"))
+    if fixTrue[:1]=="F" or fixTrue[:1]=="f" : 
+        lenTrue = str(input("Želite li da se ispisuju lozinke određene duljine? (da/ne, yes/no) :"))
+        if lenTrue[:1]=="y" or lenTrue[:1]=="d" or lenTrue[:1]=="D" or lenTrue[:1]=="Y":
+            crack2(slova, x)
+            xlen = int(input("Upišite željenu duljinu?"))
+        else: crack0(slova, x)
+        inn=False
+    else: crack1 (slova, x)
 
 print(slova)
 
